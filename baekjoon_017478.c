0e3c1311b21ce0916f4c84be52d891436ad72166
yuso <yusaei1107@gmail.com>
2022-04-21 23:15:48 +0900

    017478 2.1 업데이트

--- baekjoon_017478.c
+++ baekjoon_017478.c
@@ -18,16 +18,16 @@ int so(int n){
 	printf("라고 답변하였지.\n");
 	so(n-1);
 }											 
-int jh(int n){								
+int jh(int n){
 	for(int i=1;i<n+j;i++) printf("____");
 	printf("\"재귀함수가 뭔가요?\"\n");
-	j++;
 	if(n==0) return 1;
-	for(int i=1;i<n+j;i++) printf("____");
+	j++;												//이 j++의 위치를 감을 못 잡겠음 
+	for(int i=0;i<n+j;i++) printf("____");
 	printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
-	for(int i=1;i<n+j;i++) printf("____");
+	for(int i=0;i<n+j;i++) printf("____");
 	printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
-	for(int i=1;i<n+j;i++) printf("____");
+	for(int i=0;i<n+j;i++) printf("____");
 	printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
 	jh(n-1);
 }
