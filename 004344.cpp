80fc1557c9ed4ccc0b3e0aea7c198f6cd7c9f382
yusooo <yusaei1107@gmail.com>
2022-06-19 21:59:20 +0900

    20220619

--- 004344.cpp
+++ 004344.cpp
@@ -1,22 +1,23 @@
 #include<stdio.h>
 int main(){
 	int c, i, j, stu, sum=0;
-	scanf("%d", &c);
-	double per[c] = {0};
-	for(i = 0;i<c;i++){
-		scanf("%d", &stu);
-		int sco[j]={0};
-		for(j=0;j<stu;j++){
-			scanf("%d", &sco[j]);
-			sum+=sco[j];
+	scanf("%d", &c); // 테스트 케이스 개수 입력받기 
+	double per[c] = {0}; // 학생 비 
+	for(i=0;i<c;i++){  // 각 케이스 입력받기 
+		scanf("%d", &stu); // 학생의 수 입력받기 
+		int sco[j]={0};  // 점수 
+		for(j=0;j<stu;j++){  // 학생마다 점수 입력받기 
+			scanf("%d", &sco[j]); // 배열에 각 학생의 점수값 
+			sum+=sco[j];  	// 평균 구하기 위해 합계 
 		}
-		for(j=0;j<stu;j++){
-			if(sco[j]>(double)sum/(double)j) per[i]++;
+		for(j=0;j<stu;j++){  // 평균보다 큰 인원 구하기 
+			if(sco[j]>(double)sum/(double)j) per[i]++; // 
 		}
-		per[i]/=stu;
-		sum=0;
+		per[i]/=stu; // 평균보다 큰 인원 비율 구하기 
+		sum=0;  // 합계 초기화 
 	}
-	for(i=0;i<c;i++){
-		printf("%.3lf%%\n", per[i]);
+	for(i=0;i<c;i++){  // 각 케이스마다 비율 출력 
+		printf("%.3lf%%\n", per[i]); // 셋째자리까지 출력하기 
 	}
+	return 0; 
 }
