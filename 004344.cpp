cce759980e1d7b16fd4965515185f50ff66182c5
yusooo <yusaei1107@gmail.com>
2022-06-20 23:22:11 +0900

    20220620
    아 왜 per[i] 배열에 값 저장 안대는거임

--- 004344.cpp
+++ 004344.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	int c, i, j, stu, sum=0;
+	int c, i, j, stu, sum=0, avg=0;
 	scanf("%d", &c); // 테스트 케이스 개수 입력받기 
 	double per[c] = {0}; // 학생 비 
 	for(i=0;i<c;i++){  // 각 케이스 입력받기 
@@ -11,10 +11,10 @@ int main(){
 			sum+=sco[j];  	// 평균 구하기 위해 합계 
 		}
 		for(j=0;j<stu;j++){  // 평균보다 큰 인원 구하기 
-			if(sco[j]>(double)sum/(double)j) per[i]++; // 
+			if(sco[j]>(double)sum/(double)j) avg++; // 
 		}
-		per[i]/=stu; // 평균보다 큰 인원 비율 구하기 
-		sum=0;  // 합계 초기화 
+		per[i]=(double)avg/(double)stu; // 평균보다 큰 인원 비율 구하기 
+		sum=0, avg=0;  // 합계 초기화 
 	}
 	for(i=0;i<c;i++){  // 각 케이스마다 비율 출력 
 		printf("%.3lf%%\n", per[i]); // 셋째자리까지 출력하기 
