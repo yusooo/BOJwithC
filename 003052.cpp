105b11e380fc4496e120f4dfb7bb3e4294e2f20d
yusooo <yusaei1107@gmail.com>
2022-07-12 07:32:03 +0900

    오답

--- 003052.cpp
+++ 003052.cpp
@@ -3,7 +3,7 @@ int main(){
 	int scan[10]={0}, per[42]={0}, count=0;
 	for(int i=0;i<10;i++){
 		scanf("%d", &scan[i]);
-		per[scan[i]]++;
+		per[(scan[i]%42)-1]++;
 	}
 	for(int i=0;i<42;i++){
 		if(per[i]!=0) count++;
